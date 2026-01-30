72bf16e358eddbc503b6e4ec08537dbc9a04a99d
ExiuxD <mauricio.martinez.navarrete@gmail.com>
2026-01-29 20:50:40 -0600

    Agregamos los archivos de funciones

---
+++ Trabajos-pc/Funciones.cpp
@@ -0,0 +1,11 @@
+// funciones.cpp
+#include <iostream>
+#include "Funciones.h"
+
+void Trabajo1() {
+    std::cout << "Hola desde una funcion" << std::endl;
+}
+
+int sumar(int a, int b) {
+    return a + b;
+}
---
+++ Trabajos-pc/Funciones.h
@@ -0,0 +1,9 @@
+#pragma once
+// funciones.h
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+void Trabajo1();
+int sumar(int a, int b);
+
+#endif
